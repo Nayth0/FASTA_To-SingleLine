a0752d6eb69b5150492b7dd5a9fa99adab3f7c2e
Nathan <31522809+Nayth0@users.noreply.github.com>
2023-09-12 21:29:06 -0400

    Update FASTACMD.py

--- FASTACMD.py
+++ FASTACMD.py
@@ -1,6 +1,12 @@
 import argparse
+import os
 
-def fasta_to_single_line(input_files, output_file):
+def combine_filenames(input_files):
+    # Combine the base names of input files
+    base_names = [os.path.splitext(os.path.basename(file))[0] for file in input_files]
+    return "_".join(base_names) + "_single_line.fasta"
+
+def fasta_to_single_line(input_files, output_file=None):
     try:
         with open(output_file, 'w') as output_fasta:
             for input_file in input_files:
@@ -28,11 +34,15 @@ def fasta_to_single_line(input_files, output_file):
 def main():
     parser = argparse.ArgumentParser(description='Convert FASTA files to single-line format and combine them into a single output file.')
     parser.add_argument('-i', '--input', nargs='+', required=True, help='Input FASTA file(s)')
-    parser.add_argument('-o', '--output', required=True, help='Output FASTA file')
+    parser.add_argument('-o', '--output', help='Output FASTA file (default: combined input file names)')
 
     args = parser.parse_args()
     input_files = args.input
     output_file = args.output
+
+    if not output_file:
+        # Use the default output file name based on input file names
+        output_file = combine_filenames(input_files)
 
     fasta_to_single_line(input_files, output_file)
 
